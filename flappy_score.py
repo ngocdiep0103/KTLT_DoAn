6f84b41c8042aa0bd221c9a27178fe0015eb0df2
Tran26 <104017906+Tran26@users.noreply.github.com>
2022-04-21 23:34:05 +0700

    Thêm def score_display và update_score

---
+++ flappy_score.py
@@ -0,0 +1,23 @@
+def score_display(game_state):
+    if game_state == 'main game':
+        score_surface = game_font.render(str(int(score)), True, (255, 255, 255))
+        score_rect = score_surface.get_rect(center=(180, 100))
+        WIN.blit(score_surface, score_rect)
+    if game_state == 'game over':
+        score_surface = game_font.render(f'Score: {int(score)}', True, (255, 255, 255))
+        score_rect = score_surface.get_rect(center=(180, 100))
+        WIN.blit(score_surface, score_rect)
+
+        high_score_surface = game_font.render(f'High Score: {int(high_score)}', True, (255, 255, 255))
+        high_score_rect = score_surface.get_rect(center=(120, 525))
+        WIN.blit(high_score_surface, high_score_rect)
+
+
+def update_score(score, high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
+
+# điểm
+score = 0
+high_score = 0
