ca21d4b4843eccd57c0bf2b51ea9cfa4ce2db28e
Tran26 <104017906+Tran26@users.noreply.github.com>
2022-05-05 12:30:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ from mon import Mon
 pygame.mixer.pre_init(frequency=44100, size=-16, channels=2, buffer=512)
 pygame.init()
 idx = 0
+# đặt size cho cửa sổ
+WIN = pygame.display.set_mode((360, 640))
+
+# set up background
+bg = pygame.image.load('image/background.png').convert()
+bg = pygame.transform.scale2x(bg)
+
+# đặt tên cửa sổ
+pygame.display.set_caption("FLAPPY MON")
+
+# xác định số frame mỗi giây
+FPS = 60  # tốc dộ bình thường của game
+clock = pygame.time.Clock()
 # thêm ống
 PIPE_SURFACE = pygame.image.load('image/column.png').convert()
 PIPE_HEIGHT = [i for i in range(300, 500)]
