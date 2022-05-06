c27144a6d7e966c2b9d88348c910541615455c9f
Myakim112 <95364879+Myakim112@users.noreply.github.com>
2022-05-06 10:39:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -65,4 +65,56 @@ def gameStart(WIN):
         pygame.display.update()
         clock.tick(FPS)
             
+def main(game_active):
 
+    while True:  # tạo loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and game_active: #Tạo hiệu ứng bay
+                    # jump
+                    doramon.set_mon_movement(-5)
+                    sound.flap_sound.play()
+                if event.key == pygame.K_SPACE and game_active == False: #tạo di chuyển của ống
+                    game_active = True
+                    pipe_manager.clear_pipe_list()
+                    doramon.mon_rect.center = (100, 320)
+                    doramon.set_mon_movement(0)
+                    score.reset_score()
+                    pipe_manager.idx = 0
+            if event.type == spawnpipe: #tạo hệ thống ống
+                pipe_pos = pipe.create_pipe()
+                pipe_manager.add_pipe(pipe_pos)
+
+        if game_active:
+            WIN.blit(bg, (0, 0))  # thêm background
+            doramon.move()
+            WIN.blit(doramon.rotated_mon, doramon.mon_rect)  # thêm mon vào
+            # ống
+            pipe_manager.pipe_movement()
+            pipe_manager.draw_pipe()
+            game_active = check_collision(pipe_manager.get_pipe_list())
+            # điểm
+            score.display_score('main game')
+            floor.move_floor()
+            floor.draw_floor()
+            floor.check_and_reset_floor()
+        else:
+            # màn hình game over
+            WIN.blit(game_over_surface, game_over_rect)
+            # điểm
+            score.update_score()
+            score.display_score('game over')
+        #  thêm floor chuyển động
+        #floor.move_floor()
+        floor.draw_floor()
+        #floor.check_and_reset_floor()
+
+        pygame.display.update()
+        clock.tick(FPS)  # để kiểm soát tốc độ của chương trình
+    
+if __name__ == '__main__':
+    gameStart(WIN)
+    main(game_active)
