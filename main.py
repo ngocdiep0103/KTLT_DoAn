4ee0604227b1d76b28653c05c0f13373a6895e4d
ngocdiep0103 <95364961+ngocdiep0103@users.noreply.github.com>
2022-05-06 10:09:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -38,30 +38,3 @@ game_over_rect = game_over_surface.get_rect(center=(180, 320))
 spawnpipe = pygame.USEREVENT
 pygame.time.set_timer(spawnpipe, 1200)  # sau 1.2s tạo ra ống mới
 
-        if game_active:
-            WIN.blit(bg, (0, 0))  # thêm background
-            doramon.move()
-            WIN.blit(doramon.rotated_mon, doramon.mon_rect)  # thêm mon vào
-            # ống
-            pipe_manager.pipe_movement()
-            pipe_manager.draw_pipe()
-            game_active = check_collision(pipe_manager.get_pipe_list())
-            # điểm
-            score.display_score('main game')
-            floor.move_floor()
-            floor.draw_floor()
-            floor.check_and_reset_floor()
-        else:
-            # màn hình game over
-            WIN.blit(game_over_surface, game_over_rect)
-            # điểm
-            score.update_score()
-            score.display_score('game over')
-            xuatpsrafile('aaaa.bbb',score.high_score)
-        #  thêm floor chuyển động
-        #floor.move_floor()
-        floor.draw_floor()
-        #floor.check_and_reset_floor()
-
-        pygame.display.update()
-        clock.tick(FPS)  # để kiểm soát tốc độ của chương trình
